fc838c364221f6521c4d8c05886b946eec3ef1b4
Vadim Zapolski <v.zapolskydovnar@gmail.com>
2018-03-26 16:35:05 +0300

    External ip service support (#5)
    *  or with (port) support added
    
    * beta version added

--- bridge/bridge.go
+++ bridge/bridge.go
@@ -312,7 +312,9 @@ func (b *Bridge) newService(port ServicePort, isgroup bool) *Service {
 		service.Name += "-" + port.ExposedPort
 	}
 	var p int
-	if b.config.Internal == true {
+	var exposeExternalAddress = metadata["expose_external_address"] == "true"
+
+	if b.config.Internal == true && !exposeExternalAddress {
 		service.IP = port.ExposedIP
 		p, _ = strconv.Atoi(port.ExposedPort)
 	} else {
@@ -371,6 +373,7 @@ func (b *Bridge) newService(port ServicePort, isgroup bool) *Service {
 	delete(metadata, "id")
 	delete(metadata, "tags")
 	delete(metadata, "name")
+	delete(metadata, "expose_external_address")
 	service.Attrs = metadata
 	service.TTL = b.config.RefreshTtl
 
